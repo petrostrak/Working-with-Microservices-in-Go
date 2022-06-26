01a9844d91d5e3613e34b349bf62d62ba97a78ed
petrostrak <pit.trak@gmail.com>
2022-06-26 20:10:07 +0300

    Refactor broker to use tools package

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/petrostrak/toolbox"
 )
 
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	var tool toolbox.Tools
+
 	payload := jsonResponse{
 		Error:   false,
 		Message: "Hit the broker",
 	}
 
-	_ = app.writeJSON(w, http.StatusOK, payload)
+	_ = tool.WriteJSON(w, http.StatusOK, payload)
 }
--- broker-service/go.mod
+++ broker-service/go.mod
@@ -6,3 +6,9 @@ require (
 	github.com/go-chi/chi v1.5.4
 	github.com/go-chi/cors v1.2.1
 )
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.0 // indirect
+	github.com/petrostrak/toolbox v0.0.0-20220626163508-79a2df2e863a // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+)
