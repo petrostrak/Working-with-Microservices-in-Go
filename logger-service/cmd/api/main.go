1ee986bfed4625caac90f790911ff015ea226eee
petrostrak <pit.trak@gmail.com>
2022-06-27 15:36:54 +0300

    Connect to MongoDB

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	PORT      = "80"
+	RPC_PORT  = "5001"
+	MONGO_URL = "mongodb://mongo:27017"
+	GRPC_PORT = "50001"
+)
+
+var (
+	client *mongo.Client
+)
+
+type Config struct{}
+
+func main() {
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection Options
+	clientOptions := options.Client().ApplyURI(MONGO_URL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("error connectiong to mongoDB", err)
+		return nil, err
+	}
+
+	return c, nil
+}
