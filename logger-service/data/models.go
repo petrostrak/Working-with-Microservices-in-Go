c9598199f52fc235ec26d29b33309af80ea8fcaa
petrostrak <pit.trak@gmail.com>
2022-06-27 15:45:57 +0300

    Add mongo.New

---
+++ logger-service/data/models.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var (
+	client *mongo.Client
+)
+
+type Models struct {
+	LogEntry
+}
+
+type LogEntry struct {
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string    `bson:"name" json:"name"`
+	Data      string    `bson:"data" json:"data"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
+
+func New(mongo *mongo.Client) Models {
+	client = mongo
+
+	return Models{
+		LogEntry: LogEntry{},
+	}
+}
