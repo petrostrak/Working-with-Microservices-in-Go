0fae854e7a4eedbdc0748c87d1cf0aa5228f6082
petrostrak <pit.trak@gmail.com>
2022-06-27 15:56:00 +0300

    Add All method for mongo

--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
@@ -47,4 +49,37 @@ func (l *LogEntry) Insert(entry LogEntry) error {
 	}
 
 	return nil
+}
+
+func (l *LogEntry) All() ([]*LogEntry, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	opts := options.Find()
+	opts.SetSort(bson.D{{"created_at", -1}})
+
+	cursor, err := collection.Find(context.TODO(), bson.D{}, opts)
+	if err != nil {
+		log.Println("finding all docs error:", err)
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var logs []*LogEntry
+
+	for cursor.Next(ctx) {
+		var item LogEntry
+
+		err := cursor.Decode(&item)
+		if err != nil {
+			log.Println("Error decoding log into slice:", err)
+			return nil, err
+		}
+
+		logs = append(logs, &item)
+	}
+
+	return logs, nil
 }
