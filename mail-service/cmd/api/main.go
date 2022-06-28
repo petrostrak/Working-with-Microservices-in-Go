782758881b3fe711f2013fd59c39ca3a5fba0be5
petrostrak <pit.trak@gmail.com>
2022-06-28 13:08:04 +0300

    Setup mailer in main.go

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
@@ -15,7 +17,9 @@ const (
 )
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Println("Starting mail service on port", PORT)
 
@@ -26,5 +30,23 @@ func main() {
 
 	if err := srv.ListenAndServe(); err != nil {
 		log.Panic(err)
+	}
+}
+
+func createMail() Mail {
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		log.Println("could not parse mail port")
+	}
+
+	return Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
 	}
 }
