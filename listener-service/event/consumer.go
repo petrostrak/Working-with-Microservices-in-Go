688c4801718db510c46d62128c754f390c988280
petrostrak <pit.trak@gmail.com>
2022-06-29 17:36:35 +0300

    Add Listen to consumer logic

--- listener-service/event/consumer.go
+++ listener-service/event/consumer.go
@@ -1,6 +1,10 @@
 package event
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -30,3 +34,69 @@ func (c *Consumer) setup() error {
 
 	return declareExchange(channel)
 }
+
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (c *Consumer) Listen(topics []string) error {
+	ch, err := c.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+	if err != nil {
+		return err
+	}
+
+	for _, s := range topics {
+		err = ch.QueueBind(q.Name, s, "logs_topic", false, nil)
+		if err != nil {
+			return err
+		}
+	}
+
+	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	forever := make(chan bool)
+	go func() {
+		for d := range messages {
+			var payload Payload
+			_ = json.Unmarshal(d.Body, &payload)
+
+			go handlePayload(payload)
+		}
+	}()
+
+	fmt.Printf("Waiting for message [Exchange, Queue] [logs_topic, %s]\n", q.Name)
+
+	<-forever
+
+	return nil
+}
+
+func handlePayload(payload Payload) {
+	switch payload.Name {
+	case "log", "event":
+		// log whatever we get
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+	case "auth":
+		// authenticate
+	default:
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func logEvent(entry Payload) error
--- listener-service/event/event.go
+++ listener-service/event/event.go
@@ -15,3 +15,14 @@ func declareExchange(ch *amqp.Channel) error {
 		nil,          // arguments
 	)
 }
+
+func declareRandomQueue(ch *amqp.Channel) (amqp.Queue, error) {
+	return ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+}
