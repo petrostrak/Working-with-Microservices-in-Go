6ab6c5a772c8487df7d699e01ef7e4b5383d6f77
petrostrak <pit.trak@gmail.com>
2022-07-05 10:47:01 +0300

    Getting started with the gRPC server - WriteLog method

---
+++ logger-service/cmd/api/grpc.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"logger/data"
+	"logger/logs"
+)
+
+type LogServer struct {
+	logs.UnimplementedLogServiceServer
+	Models data.Models
+}
+
+func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.LogResponse, error) {
+	input := req.GetLogEntry()
+
+	// write the log
+	logEntry := data.LogEntry{
+		Name: input.Name,
+		Data: input.Data,
+	}
+
+	err := l.Models.LogEntry.Insert(logEntry)
+	if err != nil {
+		res := &logs.LogResponse{Result: "failed"}
+		return res, err
+	}
+
+	// return response
+	return &logs.LogResponse{Result: "logged"}, nil
+}
